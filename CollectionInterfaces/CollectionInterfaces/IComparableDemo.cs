0697a5c66da81fc3fe59c0f38b91457dc9d26401
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-29 10:55:29 +0530

    Push new Project

---
+++ CollectionInterfaces/CollectionInterfaces/IComparableDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CollectionInterfaces
+{
+    //  This program demo. how to Sort collection with IComparable Interface.
+    class IComparableDemo
+    {
+        static void Main()
+        {
+            //List<int> intList = new List<int> {11,4,6,1,33 };
+            //intList.Sort();
+            //foreach(var num in intList)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            List<SmartPhone> smartPhones = new List<SmartPhone>
+            {
+                new SmartPhone{Name="MI-Note10",Price=20000 },
+                new SmartPhone{Name="Samsung-Galaxy",Price=18000 },
+                new SmartPhone{Name="Oppo-F10",Price=10000 }
+            };
+
+            smartPhones.Sort();
+
+            foreach(var smarphone in smartPhones)
+            {
+                Console.WriteLine($"Name={smarphone.Name}\t Price={smarphone.Price}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
